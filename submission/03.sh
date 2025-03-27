46b6ccfa3067126a004b0e011d660c31ae574120
CasperDkk <daniel.kamau8@gmail.com>
2025-03-27 13:59:49 +0300

    Add changes

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,9 @@
 # Created a SegWit address.
+address=$(bitcoin-cli -regtest getnewaddress SEGWIT_ADDR bech32)
+
 # Add funds to the address.
+bitcoin-cli -regtest generatetoaddress 101 $address > /dev/null
+
 # Return only the Address
+echo "$address"
+
